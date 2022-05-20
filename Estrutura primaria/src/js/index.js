b83d5b234377e98aaec898ce67c8b73946c09b46
Thiago <slipknothiago@gmail.com>
2022-05-20 12:38:20 -0300

    finaliza o projeto

--- Estrutura primaria/src/js/index.js
+++ Estrutura primaria/src/js/index.js
@@ -12,39 +12,31 @@
     - alterar o nome do jogador 1
 */
 
-const personagens = document.querySelectorAll(".personagem");
+
+// const homemDeFerro = document.getElementById('homem-de-ferro')
+// homemDeFerro.addEventListener('mouseenter', () => {
+//   console.log('teste');
+// })
+const personagens = document.querySelectorAll('.personagem')
 
 personagens.forEach((personagem) => {
-  personagem.addEventListener("mouseenter", () => {
+  personagem.addEventListener('mouseenter', () => {
 
     const idSelecionado = personagem.attributes.id.value;
 
     if(idSelecionado === 'ultron') return;
 
-    /* 
-        OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
+    const personagemSelecionado = document.querySelector('.selecionado')
+    personagemSelecionado.classList.remove('selecionado')
+    //console.log(personagemSelecionado)
+    personagem.classList.add('selecionado');
 
-        - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele 
+    const imgPersonagemSelecionado = document.getElementById('personagem-jogador-1')
+    imgPersonagemSelecionado.src = `./Source/Imagens/${idSelecionado}.png`
 
-        - retirar a classe selecionado do personagem que está selecionado
-    */
-    const personagemSelecionado = document.querySelector(".selecionado");
-    personagemSelecionado.classList.remove("selecionado");
+    const nomeJogador1 = document.getElementById('personagem-selecionado');
+    const nomeSelecionado = personagem.getAttribute('data-base');
 
-    personagem.classList.add("selecionado");
-
-    /* 
-        OBJETIVO 2 - quando passar o mouse em cima do personagem da listagem, trocar a imagem e nome do personagem grande
-            - alterar a imagem do jogador 1
-            - alterar o nome do jogador 1
-    */
-
-    const imagemJogador1 = document.getElementById('personagem-jogador-1');
-    imagemJogador1.src = `./src/imagens/${idSelecionado}.png`;
-
-    const nomeJogador1 = document.getElementById('nome-jogador-1');
-    const nomeSelecionado = personagem.getAttribute('data-name');
-    
     nomeJogador1.innerHTML = nomeSelecionado;
-  });
-});
+  })
+})
